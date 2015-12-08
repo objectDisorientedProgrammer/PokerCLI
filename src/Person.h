a634f2cfa30af91d07de117c208a36f2b58ba364
Douglas Chidester <tikiman92@gmail.com>
2015-12-07 20:59:23 -0800

    Added Person class

---
+++ src/Person.h
@@ -0,0 +1,41 @@
+/*
+    Person.h
+    Copyright (C) 2015  Douglas Chidester
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ 
+    Created on: Dec 7, 2015
+ */
+
+#ifndef PERSON_H
+#define PERSON_H
+
+#include <iostream>
+#include <string>
+
+class Person
+{
+public:
+	Person() { name = "Frank"; }
+	Person(std::string name) { this->name = name; }
+	~Person() {}
+	
+	inline void setName(std::string newName) { name = newName; }
+	inline void greet() { std::cout << "Welcome " << name << std::endl; }
+protected:
+	std::string name;
+};
+
+#endif
+
